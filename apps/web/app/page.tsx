b5309878a55c41efee1ad3b9275df750ea8960e6
sandraiconiqueintern <sandra@iconique.com.ph>
2026-06-19 17:35:22 +0800

    feat: implement CRUD operations for todos and update nodemon configuration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,4 @@
+//apps/web/app/page.tsx
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,9 @@
 export function sharedUtil() {
     console.log("This is a shared utility function");
 }
+
+export type Todo = {
+  id: number;
+  todo: string;
+};
+
